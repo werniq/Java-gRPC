0ae31b8bf043fdc69577963891d55bfe6cbae75c
werniq <qniwwwersss@gmail.com>
2023-12-09 11:57:36 +0100

    gRPC User & Order Services.

---
+++ Start/shopping-services/src/main/java/com/shopping/client/OrderClient.java
@@ -0,0 +1,26 @@
+package com.shopping.client;
+
+import com.shopping.stubs.order.Order;
+import com.shopping.stubs.order.OrderRequest;
+import com.shopping.stubs.order.OrderResponse;
+import com.shopping.stubs.order.OrderServiceGrpc;
+import io.grpc.Channel;
+
+import java.util.List;
+
+public class OrderClient {
+    // get a stub object
+    // call service method
+
+    private OrderServiceGrpc.OrderServiceBlockingStub orderServiceBlockingStub;
+
+    public OrderClient(Channel channel){
+        orderServiceBlockingStub = OrderServiceGrpc.newBlockingStub(channel);
+        }
+
+    public List<Order> getOrders(int userId){
+        OrderRequest orderRequest = OrderRequest.newBuilder().setUserId(userId).build();
+       OrderResponse orderResponse =  orderServiceBlockingStub.getOrdersForUser(orderRequest);
+       return orderResponse.getOrderList();
+    }
+}
